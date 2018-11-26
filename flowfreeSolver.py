8cbc9d07f7348207d2619efdf50c941b2a84126f
Heng <lhengi888@gmail.com>
2018-11-25 23:51:19 -0600

    still something wrong

--- flowfreeSolver.py
+++ flowfreeSolver.py
@@ -16,7 +16,14 @@ NO
     [0,0,0,1,0],
     [0,3,0,0,2],
     [0,0,0,0,0]]
-    
+
+YES
+[
+[1,0,2],
+[0,0,0],
+[1,0,2]
+]
+
 NO
     [
     [1,0,2],
@@ -25,11 +32,11 @@ NO
     ]
 """
 arr = [
-    [1,0,0,3,0],
-    [2,0,0,0,0],
-    [0,0,0,1,0],
-    [0,3,0,0,2],
-    [0,0,0,0,0]]
+[1,0,2],
+[0,0,0],
+[1,0,2]
+]
+
 
 def construct3Dbool(arr,colorList):
     # create a 3d matrix of boolean vars
@@ -39,24 +46,58 @@ def construct3Dbool(arr,colorList):
 
     for i in range(0,numColors):
         eachColorM = []
-        for j in range(0,n):
-            eachColorM.append(BoolVector("m"+str(i)+"_"+str(j),n))
+        for j in range(0,n+2):
+            eachColorM.append(BoolVector("m"+str(i)+"_"+str(j),n+2))
         m.append(eachColorM)
+
+
     return m
 
-def constructDistinctRule(arr,colorList,m):
+def transformInput(arr,colorList):
+
+    # add 0 edges and set the to false to later, so we don't need to handle edge cases
+    processedArr = []
+    for k in colorList:
+        colorM = []
+        colorM.append([0]*(len(arr)+2))
+        for i in range(0,len(arr)):
+            colorRow = [0]
+            for j in range(0,len(arr[0])):
+                colorRow.append(1 if arr[i][j] == k else 0)
+            colorRow.append(0)
+            colorM.append(colorRow)
+        colorM.append([0] * (len(arr) + 2))
+        processedArr.append(colorM)
+
+    return processedArr
+def getColorList(arr):
+    numColor = {0}
+    for i in range(0, len(arr)):
+        numColor = numColor.union(set(arr[i]))
+
+    colorList = list(numColor)
+    colorList.sort()
+    colorList.pop(0)  # take out color 0
+    return colorList
+
+def constructDistinctRule(colorList,m):
     #
-    n = len(arr)
+    n = len(m[0])
     numColors = len(colorList)
     colorClause = []
 
     print(len(m),"  ",len(m[0]),"  ",len(m[0][0]))
-    for k in range(0,numColors-1): # each color
-        for i in range(0,n): # each row
+    for k in range(0,numColors): # each color
+        for i in range(0,n ): # each row
             for j in range(0,n): # each colum
-                for k2 in range(k+1,numColors): # each color
+                if i == 0 or j == 0 or i == n - 1 or j == n -1:
+                    colorClause.append(Not(m[k][i][j]))
+                    continue
+                for k2 in range(0,numColors): # each color
                     #print("K : ", k, " i: ", i, " j: ", j)
                     #print("K2: ", k2, " i: ", i, " j: ", j)
+                    if k == k2:
+                        continue
                     colorClause.append( Or(Not(m[k][i][j]), m[k2][i][j])) # color in this cell implies no other color in other cells
 
 
@@ -64,13 +105,15 @@ def constructDistinctRule(arr,colorList,m):
     
 
 
+
 def constructcolorCellRule(k,matrix,currentColorMatrix):
     # currentColorMatrix should only contains 0 and 1, where 1 is start and end
-    n = len(matrix)
+    n = len(matrix[0])
     clauses = []
 
-    for i in range(0,n):
-        for j in range(0,n):
+    for i in range(1,n-1):
+
+        for j in range(1,n-1):
             if currentColorMatrix[i][j] == 1:
 
                 clauses.append( Or( matrix[k][i + 1][j], matrix[k][i - 1][j] , matrix[k][i][j + 1], matrix[k][i][j - 1] ) )
@@ -109,6 +152,13 @@ def constructcolorCellRule(k,matrix,currentColorMatrix):
                     (c -> d) -> -a
                     (c -> d) -> -b
                 """
+                print("k: ",k," i: ",i," j: ",j)
+
+                # need to consider edge cases
+                a = matrix[k][i + 1][j]
+                b = matrix[k][i - 1][j]
+                c = matrix[k][i][j + 1]
+                d = matrix[k][i][j - 1]
 
                 clauses.append( Or( matrix[k][i + 1][j], matrix[k][i - 1][j], matrix[k][i][j + 1], matrix[k][i][j - 1] ))
 
@@ -134,29 +184,6 @@ def constructcolorCellRule(k,matrix,currentColorMatrix):
     return clauses
 
 
-def transformInput(arr,colorList):
-
-    processedArr = []
-    for k in colorList:
-        colorM = []
-        for i in range(0,len(arr)):
-            colorRow = []
-            for j in range(0,len(arr[0])):
-                colorRow.append(1 if arr[i][j] == k else 0)
-            colorM.append(colorRow)
-        processedArr.append(colorM)
-
-    return processedArr
-def getColorList(arr):
-    numColor = {0}
-    for i in range(0, len(arr)):
-        numColor = numColor.union(set(arr[i]))
-
-    colorList = list(numColor)
-    colorList.sort()
-    colorList.pop(0)  # take out color 0
-    print(colorList)
-    return colorList
 
 def constructFormula(arr):
     colorList = getColorList(arr)
@@ -165,7 +192,7 @@ def constructFormula(arr):
     for i in processedArr:
         print(i)
     m = construct3Dbool(arr,colorList)
-    distinctRule = constructDistinctRule(arr,colorList,m)
+    distinctRule = constructDistinctRule(colorList,m)
     colorCellRule = []
 
     for k in range(0,len(colorList)):
@@ -174,8 +201,9 @@ def constructFormula(arr):
     s = Solver()
 
     for rule in distinctRule:
-        s.add(rule)
+        s.add(rule) # should be fine
     for rule in colorCellRule:
+        print(rule)
         s.add(rule)
 
     return s
